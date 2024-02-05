03d80b22ab1c98a72236e4a500a3d94a2e520460
Marina-Gouveia <marinagouveia420@gmail.com>
2024-02-05 05:22:06 -0300

    Consulta tabela Fipe

---
+++ src/main/java/com/alura/consultaTabelaFipe/consultaTabelaFipe/model/Dados.java
@@ -0,0 +1,5 @@
+package com.alura.consultaTabelaFipe.consultaTabelaFipe.model;
+
+public record Dados(String codigo, String nome) {
+
+}
---
+++ src/main/java/com/alura/consultaTabelaFipe/consultaTabelaFipe/model/Modelo.java
@@ -0,0 +1,9 @@
+package com.alura.consultaTabelaFipe.consultaTabelaFipe.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Modelo(List<Dados> modelos) {
+}
